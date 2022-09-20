a4df82e257aa2f9bf568148f5012b9bf43114f09
Erik <hofman.erik@outlook.com>
2022-09-20 14:12:03 +0200

    'update'

--- HelloWorld/HelloWorld/src/com/gunner2501/Main.java
+++ HelloWorld/HelloWorld/src/com/gunner2501/Main.java
@@ -1,7 +1,7 @@
 package com.gunner2501;
 
+import java.awt.*;
 import java.util.Scanner;
-
 import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.util.Arrays;
@@ -9,13 +9,24 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Age: ");
-        long age = scanner.nextByte();
-        if (age < 100) {
-            System.out.println("You are "+age);
-        }else{
-            System.out.println("You are to old");
-        }
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments)-1);
+
+        System.out.println("Mortgage: $"+ mortgage);
     }
 }
