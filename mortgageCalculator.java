9908460f2483fcce0104100f4f9f985128927fe2
Erik <hofman.erik@outlook.com>
2022-09-20 14:16:14 +0200

    'added'

---
+++ mortgageCalculator.java
@@ -0,0 +1,32 @@
+package com.gunner2501;
+
+import java.awt.*;
+import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments)-1);
+
+        System.out.println("Mortgage: $"+ mortgage);
+    }
+}
